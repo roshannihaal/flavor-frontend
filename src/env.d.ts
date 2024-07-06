c82bf42311e8a381438bb675799f2cb0dd8bd42d
roshannihaal <roshan.nihaal18@gmail.com>
2024-07-06 16:18:28 +0530

    Added husky

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,10 +1,10 @@
 // Define the type of the environment variables.
 declare interface Env {
-  readonly NODE_ENV: string;
-  readonly FL_BACKEND_URL: string;
-  // Replace the following with your own environment variables.
-  // Example: NGX_VERSION: string;
-  [key: string]: any;
+    readonly NODE_ENV: string;
+    readonly FL_BACKEND_URL: string;
+    // Replace the following with your own environment variables.
+    // Example: NGX_VERSION: string;
+    [key: string]: any;
 }
 
 // Choose how to access the environment variables.
@@ -12,7 +12,7 @@ declare interface Env {
 
 // 1. Use import.meta.env.YOUR_ENV_VAR in your code. (conventional)
 declare interface ImportMeta {
-  readonly env: Env;
+    readonly env: Env;
 }
 
 // 2. Use _NGX_ENV_.YOUR_ENV_VAR in your code. (customizable)
@@ -23,6 +23,6 @@ declare interface ImportMeta {
 declare const _NGX_ENV_: Env;
 
 // 3. Use process.env.YOUR_ENV_VAR in your code. (deprecated)
-declare namespace NodeJS {
-  export interface ProcessEnv extends Env {}
-}
+// declare namespace NodeJS {
+//   export interface ProcessEnv extends Env {}
+// }
